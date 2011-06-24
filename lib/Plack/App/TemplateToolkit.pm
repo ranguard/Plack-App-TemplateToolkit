79fee017d9413ecafb5a443c78587b7be88dfe58
Leo Lapworth <leo@cuckoo.org>
2011-06-24 21:57:32 +0100

    Revert to just before release and then depreciate

--- lib/Plack/App/TemplateToolkit.pm
+++ lib/Plack/App/TemplateToolkit.pm
@@ -95,106 +95,24 @@ sub _handle_tt {
 
 1;
 
+# ABSTRACT: DEPRECIATED use Plack::Middleware::TemplateToolkit
+
 __END__
 
 =head1 NAME
 
-Plack::App::TemplateToolkit - Basic Plack App Template Toolkit
+Plack::App::TemplateToolkit DEPRECIATED use Plack::Middleware::TemplateToolkit
 
 =head1 SYNOPSIS
 
-    # in app.psgi
-    use Plack::Builder;
-    use Plack::App::TemplateToolkit;
-
-    my $root = '/path/to/htdocs/';
-
-    my $tt_app = Plack::App::TemplateToolkit->new(
-        root => $root,    # Required
-    )->to_app();
-
-    return builder {
-
-        # Page to show when requested file is missing
-        enable "Plack::Middleware::ErrorDocument",
-            404 => "$root/page_not_found.html";
-
-        # These files can be served directly
-        enable "Plack::Middleware::Static",
-            path => qr{[gif|png|jpg|swf|ico|mov|mp3|pdf|js|css]$},
-            root => $root;
-
-        # Our application
-        $tt_app;
-    }
+  Use Plack::Middleware::TemplateToolkit instead of this.
 
 =head1 DESCRIPTION
 
-Plack::App::TemplateToolkit - process files through L<Template> Toolkit (TT)
-
-The idea behind this module is to provide access to L<Template> Toolkit (TT) for
-content that is ALMOST static, but where having the power of TT can make
-the content easier to manage. You probably only want to use this for the
-simpliest of sites, but it should be easy enough to migrate to something
-more significant later.
-
-The QUERY_STRING params are available to the templates, but the more you use
-these the harder it could be to migrate later so you might want to
-look at a propper framework such as L<Catalyst> if you do want to use them:
-
-  [% params.get('field') %] params is a L<Hash::MultiValue>
-
-You can mix this application with other Plack::App applications and
-Plack::Middleware which you will find on CPAN.
-
-=head1 CONFIGURATIONS
-
-=over 4
-
-=item root
-
-Required, root where templates live, e.g. docroot, this can be
-an array reference or a string.
-
-=item extension
-
-Limit to only files with this extension.
-
-=item content_type
-
-Specify the Content-Type header you want returned, defaults to text/html
-
-=item dir_index
-
-Which file to use as a directory index, defaults to index.html
-
-=item pre_process
-
-Optional, supply a file to pre process before serving each html file
-(see C<Template> configuration PRE_PROCESS)
-
-=item process
-
-Optional, supply a file to process (see C<Template> configuration PROCESS)
-
-=item eval_perl
-
-Default to 0, this option lets you run perl blocks in your
-templates - I would strongly recommend NOT using this.
-(see C<Template> configuration EVAL_PERL)
-
-=item interpolate
-
-Default to 0, see C<Template> configuration INTERPOLATE
-
-=item post_chomp
-
-Defaults to 1, see C<Template> configuration POST_CHOMP
-
-=back
+Use Plack::Middleware::TemplateToolkit instead of this package.
 
 =head1 SEE ALSO
 
-L<Plack>, L<Template>
+L<Plack::Middleware::TemplateToolkit>
 
 =cut
